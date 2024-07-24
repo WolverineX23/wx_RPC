5b2ec6ee7a37160cc68437f32050350af60df04d
WolverineX23 <547036318@qq.com>
2024-07-24 22:26:33 +0800

    add: 服务消费者初始化，TODO-获取服务提供者的实现类对象

---
+++ example-consumer/src/main/java/com/wx/example/consumer/EasyConsumerExample.java
@@ -0,0 +1,25 @@
+package com.wx.example.consumer;
+
+import com.wx.example.common.model.User;
+import com.wx.example.common.service.UserService;
+
+/**
+ * 简易服务消费者示例
+ */
+public class EasyConsumerExample {
+
+    public static void main(String[] args) {
+        // todo 需要获取 UserService 的实现类对象
+        UserService userService = null;
+        User user = new User();
+        user.setName("wx");
+
+        // 调用
+        User newUser = userService.getUser(user);
+        if (newUser != null) {
+            System.out.println(newUser.getName());
+        } else {
+            System.out.println("user == null");
+        }
+    }
+}
