219b48988df1950dbe773fee3b2927e68762c247
WolverineX23 <547036318@qq.com>
2024-07-24 22:19:11 +0800

    add: 服务提供者初始化，实现服务接口

---
+++ example-provider/src/main/java/com/wx/example/provider/UserServiceImpl.java
@@ -0,0 +1,18 @@
+package com.wx.example.provider;
+
+import com.wx.example.common.model.User;
+import com.wx.example.common.service.UserService;
+
+/**
+ * 用户服务实现类
+ *
+ */
+public class UserServiceImpl implements UserService {
+
+
+    @Override
+    public User getUser(User user) {
+        System.out.println("用户名：" + user.getName());
+        return user;
+    }
+}
