cf6ea8c43711ff514214b7b5b7a9c0feef617de8
WolverineX23 <547036318@qq.com>
2024-07-28 10:47:29 +0800

    add: 添加 本地服务注册器

--- example-provider/src/main/java/com/wx/example/provider/EasyProviderExample.java
+++ example-provider/src/main/java/com/wx/example/provider/EasyProviderExample.java
@@ -1,5 +1,7 @@
 package com.wx.example.provider;
 
+import com.wx.example.common.service.UserService;
+import com.wx.rpc.registry.LocalRegistry;
 import com.wx.rpc.server.HttpServer;
 import com.wx.rpc.server.VertxHttpServer;
 
@@ -9,6 +11,9 @@ import com.wx.rpc.server.VertxHttpServer;
 public class EasyProviderExample {
 
     public static void main(String[] args) {
+        // 注册服务
+        LocalRegistry.register(UserService.class.getName(), UserServiceImpl.class);
+
         // 启动 web 服务
         HttpServer httpServer = new VertxHttpServer();
         httpServer.doStart(8080);
