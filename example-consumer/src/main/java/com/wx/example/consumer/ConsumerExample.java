58991b7b508b423fb12292621db87bbccfe38dfe
WolverineX23 <547036318@qq.com>
2024-07-30 23:32:20 +0800

    test: 添加 rpc.mock 配置项，测试 mock 代理服务

--- example-consumer/src/main/java/com/wx/example/consumer/ConsumerExample.java
+++ example-consumer/src/main/java/com/wx/example/consumer/ConsumerExample.java
@@ -1,6 +1,9 @@
 package com.wx.example.consumer;
 
+import com.wx.example.common.model.User;
+import com.wx.example.common.service.UserService;
 import com.wx.rpc.config.RpcConfig;
+import com.wx.rpc.proxy.ServiceProxyFactory;
 import com.wx.rpc.utils.ConfigUtils;
 
 /**
@@ -10,10 +13,27 @@ import com.wx.rpc.utils.ConfigUtils;
 public class ConsumerExample {
 
     public static void main(String[] args) {
-        // 加载 RPC
+        /* 加载 RPC
         RpcConfig rpc = ConfigUtils.loadConfig(RpcConfig.class, "rpc");
         System.out.println(rpc);
 
         // 服务调用
+         */
+
+        // 获取代理
+        UserService userService = ServiceProxyFactory.getProxy(UserService.class);
+        User user = new User();
+        user.setName("wx");
+
+        // 调用
+        User newUser = userService.getUser(user);
+        if (newUser != null) {
+            System.out.println("调用了真实服务" + newUser.getName());
+        } else {
+            System.out.println("调用了模拟服务：user == null");
+        }
+
+        short num = userService.getNumber();
+        System.out.println(num);
     }
 }
