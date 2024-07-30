9e51ae6ed1e7a238f46054aee3c4f33426d076e6
WolverineX23 <547036318@qq.com>
2024-07-30 23:20:20 +0800

    add: 创建 Mock 服务动态代理， 添加 mock 配置项

--- wx-rpc-core/src/main/java/com/wx/rpc/proxy/ServiceProxyFactory.java
+++ wx-rpc-core/src/main/java/com/wx/rpc/proxy/ServiceProxyFactory.java
@@ -1,5 +1,7 @@
 package com.wx.rpc.proxy;
 
+import com.wx.rpc.RpcApplication;
+
 import java.lang.reflect.Proxy;
 
 /**
@@ -17,11 +19,30 @@ public class ServiceProxyFactory {
      * @return
      */
     public static <T> T getProxy(Class<T> serviceClass) {
+        if (RpcApplication.getRpcConfig().isMock()) {
+            return getMockProxy(serviceClass);
+        }
+
         // 创建代理对象 - Proxy.newProxyInstance
         return (T) Proxy.newProxyInstance(
                 serviceClass.getClassLoader(),
                 new Class[]{serviceClass},      // 只能是接口
                 new ServiceProxy()              // InvocationHandler 实现
+        );
+    }
+
+    /**
+     * 更具服务类获取 Mock 代理对象
+     *
+     * @param serviceClass
+     * @param <T>
+     * @return
+     */
+    public static <T> T getMockProxy(Class<T> serviceClass) {
+        return (T) Proxy.newProxyInstance(
+                serviceClass.getClassLoader(),
+                new Class[]{serviceClass},
+                new MockServiceProxy()
         );
     }
 }
