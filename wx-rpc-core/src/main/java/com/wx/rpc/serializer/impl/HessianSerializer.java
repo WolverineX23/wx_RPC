d9747bb74011126c6245ec125f071e576b3e8c2d
WolverineX23 <547036318@qq.com>
2024-07-31 22:04:41 +0800

    add: 实现 hessian 序列化器

---
+++ wx-rpc-core/src/main/java/com/wx/rpc/serializer/impl/HessianSerializer.java
@@ -0,0 +1,31 @@
+package com.wx.rpc.serializer.impl;
+
+import com.caucho.hessian.io.HessianInput;
+import com.caucho.hessian.io.HessianOutput;
+import com.wx.rpc.serializer.Serializer;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+
+/**
+ * Hessian 序列化器
+ */
+public class HessianSerializer implements Serializer {
+    @Override
+    public <T> byte[] serializer(T object) throws IOException {
+        ByteArrayOutputStream bos = new ByteArrayOutputStream();
+        HessianOutput ho = new HessianOutput(bos);
+        ho.writeObject(object);
+
+        return bos.toByteArray();
+    }
+
+    @Override
+    public <T> T deserializer(byte[] bytes, Class<T> type) throws IOException {
+        ByteArrayInputStream bis = new ByteArrayInputStream(bytes);
+        HessianInput hi = new HessianInput(bis);
+
+        return (T) hi.readObject(type);
+    }
+}
