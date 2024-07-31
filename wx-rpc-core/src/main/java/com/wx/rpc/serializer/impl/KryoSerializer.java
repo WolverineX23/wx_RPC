6a98289b65659af0d99806c668099b43ff41fc0f
WolverineX23 <547036318@qq.com>
2024-07-31 21:59:15 +0800

    add: 实现 kryo 序列化器

---
+++ wx-rpc-core/src/main/java/com/wx/rpc/serializer/impl/KryoSerializer.java
@@ -0,0 +1,49 @@
+package com.wx.rpc.serializer.impl;
+
+import com.esotericsoftware.kryo.Kryo;
+import com.esotericsoftware.kryo.io.Input;
+import com.esotericsoftware.kryo.io.Output;
+import com.wx.rpc.serializer.Serializer;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+
+/**
+ * Kryo 序列化器
+ */
+public class KryoSerializer implements Serializer {
+
+    /**
+     * Kryo 本身是线程不安全的，所以需要使用 ThreadLocal 保证每个线程有一个单独的 Kryo 对象实例
+     */
+    private static final ThreadLocal<Kryo> KRYO_THREAD_LOCAL = ThreadLocal.withInitial(() -> {
+        Kryo kryo = new Kryo();
+
+        // 设置动态序列化和反序列化类，不提前注册所有类（可能有安全问题）
+        kryo.setRegistrationRequired(false);
+
+        return kryo;
+    });
+
+
+    @Override
+    public <T> byte[] serializer(T object) throws IOException {
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        Output output = new Output(byteArrayOutputStream);
+        KRYO_THREAD_LOCAL.get().writeObject(output, object);
+        output.close();
+
+        return byteArrayOutputStream.toByteArray();
+    }
+
+    @Override
+    public <T> T deserializer(byte[] bytes, Class<T> type) throws IOException {
+        ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(bytes);
+        Input input = new Input(byteArrayInputStream);
+        T result = KRYO_THREAD_LOCAL.get().readObject(input, type);
+        input.close();
+
+        return result;
+    }
+}
