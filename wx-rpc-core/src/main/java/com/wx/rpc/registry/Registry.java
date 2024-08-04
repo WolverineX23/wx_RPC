c8e14e0eed9edf789eda058763d26b39dd8e412f
WolverineX23 <547036318@qq.com>
2024-08-04 15:34:06 +0800

    add: 注册中心接口 - 可扩展性设计

---
+++ wx-rpc-core/src/main/java/com/wx/rpc/registry/Registry.java
@@ -0,0 +1,51 @@
+package com.wx.rpc.registry;
+
+import com.wx.rpc.config.RegistryConfig;
+import com.wx.rpc.model.ServiceMetaInfo;
+
+import java.util.List;
+
+/**
+ * 注册中心接口
+ *
+ * 遵循可扩展设计，先写该注册中心接口，后续可实现多种不同的注册中心
+ * 和序列化器一样，可使用 SPI 机制动态加载
+ */
+public interface Registry {
+
+    /**
+     * 初始化
+     *
+     * @param registryConfig
+     */
+    void init(RegistryConfig registryConfig);
+
+    /**
+     * 注册服务（服务端）
+     *
+     * @param serviceMetaInfo
+     * @throws Exception
+     */
+    void register(ServiceMetaInfo serviceMetaInfo) throws Exception;
+
+    /**
+     * 注销服务（服务端）
+     *
+     * @param serviceMetaInfo
+     */
+    void unRegister(ServiceMetaInfo serviceMetaInfo);
+
+    /**
+     * 服务发现（获取某服务的所有节点，消费端）
+     *
+     * @param serviceKey
+     * @return
+     */
+    List<ServiceMetaInfo> serviceDiscovery(String serviceKey);
+
+    /**
+     * 服务销毁
+     *
+     */
+    void destroy();
+}
