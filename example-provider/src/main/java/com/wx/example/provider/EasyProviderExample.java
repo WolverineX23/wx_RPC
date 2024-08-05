4bdfca753e10814708ca2215d8adc9ffb80b06da
WolverineX23 <547036318@qq.com>
2024-08-05 22:22:05 +0800

    test: 服务提供者实现服务注册，消费者调用测试

--- example-provider/src/main/java/com/wx/example/provider/EasyProviderExample.java
+++ example-provider/src/main/java/com/wx/example/provider/EasyProviderExample.java
@@ -2,7 +2,12 @@ package com.wx.example.provider;
 
 import com.wx.example.common.service.UserService;
 import com.wx.rpc.RpcApplication;
+import com.wx.rpc.config.RegistryConfig;
+import com.wx.rpc.config.RpcConfig;
+import com.wx.rpc.model.ServiceMetaInfo;
 import com.wx.rpc.registry.LocalRegistry;
+import com.wx.rpc.registry.Registry;
+import com.wx.rpc.registry.RegistryFactory;
 import com.wx.rpc.server.HttpServer;
 import com.wx.rpc.server.VertxHttpServer;
 
@@ -15,11 +20,28 @@ public class EasyProviderExample {
         // RPC 框架初始化
         RpcApplication.init();
 
-        // 注册服务
-        LocalRegistry.register(UserService.class.getName(), UserServiceImpl.class);
+        String serviceName = UserService.class.getName();
+
+        // 注册服务到本地
+        LocalRegistry.register(serviceName, UserServiceImpl.class);
+
+        // 注册服务到注册中心
+        RpcConfig rpcConfig = RpcApplication.getRpcConfig();
+        RegistryConfig registryConfig = rpcConfig.getRegistryConfig();
+        Registry registry = RegistryFactory.getInstance(registryConfig.getRegistry());
+        ServiceMetaInfo serviceMetaInfo = new ServiceMetaInfo();
+        serviceMetaInfo.setServiceName(serviceName);
+        serviceMetaInfo.setServiceHost(rpcConfig.getServerHost());
+        serviceMetaInfo.setServicePort(rpcConfig.getServerPort());
+        try {
+            registry.register(serviceMetaInfo);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
 
         // 启动 web 服务
         HttpServer httpServer = new VertxHttpServer();
-        httpServer.doStart(8080);
+//        httpServer.doStart(8080);
+        httpServer.doStart(RpcApplication.getRpcConfig().getServerPort());
     }
 }
