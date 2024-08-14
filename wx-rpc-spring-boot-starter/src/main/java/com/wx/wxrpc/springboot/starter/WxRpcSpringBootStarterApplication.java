3545c1c4aa3545e950221088bbfdeac14ccd52e4
WolverineX23 <547036318@qq.com>
2024-08-14 23:06:59 +0800

    init：Spring Boot Starter 项目初始化

---
+++ wx-rpc-spring-boot-starter/src/main/java/com/wx/wxrpc/springboot/starter/WxRpcSpringBootStarterApplication.java
@@ -0,0 +1,13 @@
+package com.wx.wxrpc.springboot.starter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WxRpcSpringBootStarterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WxRpcSpringBootStarterApplication.class, args);
+    }
+
+}
---
+++ wx-rpc-spring-boot-starter/src/test/java/com/wx/wxrpc/springboot/starter/WxRpcSpringBootStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wx.wxrpc.springboot.starter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WxRpcSpringBootStarterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
