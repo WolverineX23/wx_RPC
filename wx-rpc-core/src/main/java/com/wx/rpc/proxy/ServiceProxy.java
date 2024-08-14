f79ae82676b0c792e26dc0fb8b40eb6ac7f41c58
WolverineX23 <547036318@qq.com>
2024-08-14 20:58:48 +0800

    add：服务消费端代理应用容错功能

--- wx-rpc-core/src/main/java/com/wx/rpc/proxy/ServiceProxy.java
+++ wx-rpc-core/src/main/java/com/wx/rpc/proxy/ServiceProxy.java
@@ -9,6 +9,8 @@ import com.wx.rpc.config.RpcConfig;
 import com.wx.rpc.constant.RpcConstant;
 import com.wx.rpc.fault.retry.RetryStrategy;
 import com.wx.rpc.fault.retry.RetryStrategyFactory;
+import com.wx.rpc.fault.tolerant.TolerantStrategy;
+import com.wx.rpc.fault.tolerant.TolerantStrategyFactory;
 import com.wx.rpc.loadbalancer.LoadBalancer;
 import com.wx.rpc.loadbalancer.LoadBalancerFactory;
 import com.wx.rpc.model.RpcRequest;
@@ -72,39 +74,45 @@ public class ServiceProxy implements InvocationHandler {
                 .args(args)
                 .build();
 
-        try {
-            // 从注册中心获取服务提供者请求地址
-            RpcConfig rpcConfig = RpcApplication.getRpcConfig();
-            Registry registry = RegistryFactory.getInstance(rpcConfig.getRegistryConfig().getRegistry());
-            ServiceMetaInfo serviceMetaInfo = new ServiceMetaInfo();
-            serviceMetaInfo.setServiceName(serviceName);
-            serviceMetaInfo.setServiceVersion(RpcConstant.DEFAULT_SERVICE_VERSION);
-            List<ServiceMetaInfo> serviceMetaInfoList = registry.serviceDiscovery(serviceMetaInfo.getServiceKey());
-            if (CollUtil.isEmpty(serviceMetaInfoList)) {
-                throw new RuntimeException("暂无服务地址");
-            }
+        // 从注册中心获取服务提供者请求地址
+        RpcConfig rpcConfig = RpcApplication.getRpcConfig();
+        Registry registry = RegistryFactory.getInstance(rpcConfig.getRegistryConfig().getRegistry());
+        ServiceMetaInfo serviceMetaInfo = new ServiceMetaInfo();
+        serviceMetaInfo.setServiceName(serviceName);
+        serviceMetaInfo.setServiceVersion(RpcConstant.DEFAULT_SERVICE_VERSION);
+        List<ServiceMetaInfo> serviceMetaInfoList = registry.serviceDiscovery(serviceMetaInfo.getServiceKey());
+        if (CollUtil.isEmpty(serviceMetaInfoList)) {
+            throw new RuntimeException("暂无服务地址");
+        }
 
-            // 负载均衡
-            LoadBalancer loadBalancer = LoadBalancerFactory.getInstance(rpcConfig.getLoadBalancer());
+        // 负载均衡
+        LoadBalancer loadBalancer = LoadBalancerFactory.getInstance(rpcConfig.getLoadBalancer());
 
-            // 将调用方法名（请求路径）作为负载均衡参数 - 若使用 一致性 Hash 负载均衡器，则调用相同方法的请求总会打到同一个服务器节点上
-            Map<String, Object> requestParams = new HashMap<>();
-            requestParams.put("methodName", rpcRequest.getMethodName());
-            ServiceMetaInfo selectedServiceMetaInfo = loadBalancer.select(requestParams, serviceMetaInfoList);
+        // 将调用方法名（请求路径）作为负载均衡参数 - 若使用 一致性 Hash 负载均衡器，则调用相同方法的请求总会打到同一个服务器节点上
+        Map<String, Object> requestParams = new HashMap<>();
+        requestParams.put("methodName", rpcRequest.getMethodName());
+        ServiceMetaInfo selectedServiceMetaInfo = loadBalancer.select(requestParams, serviceMetaInfoList);
 
 //            ServiceMetaInfo selectedServiceMetaInfo = serviceMetaInfoList.get(0);
 
+        // rpc 请求
+        RpcResponse rpcResponse;
+        try {
             // 发送 TCP 请求， 获取响应结果
 //            RpcResponse rpcResponse = VertxTcpClient.doRequest(rpcRequest, selectedServiceMetaInfo);
             // 使用 重试机制
             RetryStrategy retryStrategy = RetryStrategyFactory.getInstance(rpcConfig.getRetryStrategy());
-            RpcResponse rpcResponse = retryStrategy.doRetry(() ->
+            rpcResponse = retryStrategy.doRetry(() ->
                 VertxTcpClient.doRequest(rpcRequest, selectedServiceMetaInfo)
             );
-            return rpcResponse.getData();
         } catch (Exception e) {
+            // 容错机制
+            TolerantStrategy tolerantStrategy = TolerantStrategyFactory.getInstance(rpcConfig.getTolerantStrategy());
+            rpcResponse = tolerantStrategy.doTolerant(null, e);
             throw new RuntimeException("调用失败");
         }
+
+        return rpcResponse.getData();
     }
 
     /**
