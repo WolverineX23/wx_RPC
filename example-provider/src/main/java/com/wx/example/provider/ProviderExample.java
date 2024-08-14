7806821bb03074587e48b0e5d7a04b4e4b5a6946
WolverineX23 <547036318@qq.com>
2024-08-14 22:13:22 +0800

    add：服务提供者启动类实现和应用

--- example-provider/src/main/java/com/wx/example/provider/ProviderExample.java
+++ example-provider/src/main/java/com/wx/example/provider/ProviderExample.java
@@ -2,13 +2,18 @@ package com.wx.example.provider;
 
 import com.wx.example.common.service.UserService;
 import com.wx.rpc.RpcApplication;
+import com.wx.rpc.bootstrap.ProviderBootstrap;
 import com.wx.rpc.config.RegistryConfig;
 import com.wx.rpc.config.RpcConfig;
 import com.wx.rpc.model.ServiceMetaInfo;
+import com.wx.rpc.model.ServiceRegisterInfo;
 import com.wx.rpc.registry.LocalRegistry;
 import com.wx.rpc.registry.Registry;
 import com.wx.rpc.registry.RegistryFactory;
 import com.wx.rpc.server.tcp.VertxTcpServer;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * 服务提供者示例
@@ -17,6 +22,17 @@ import com.wx.rpc.server.tcp.VertxTcpServer;
 public class ProviderExample {
 
     public static void main(String[] args) {
+
+        // 启动机制 rpc 框架启动代码
+        // 要注册的服务
+        List<ServiceRegisterInfo<?>> serviceRegisterInfoList = new ArrayList<>();
+        ServiceRegisterInfo<UserService> serviceRegisterInfo = new ServiceRegisterInfo<>(UserService.class.getName(), UserServiceImpl.class);
+        serviceRegisterInfoList.add(serviceRegisterInfo);
+
+        // 服务提供者初始化
+        ProviderBootstrap.init(serviceRegisterInfoList);
+
+        /* old usage method
         // RPC 框架初始化
         RpcApplication.init();
 
@@ -43,5 +59,6 @@ public class ProviderExample {
         // 启动 TCP 服务
         VertxTcpServer vertxTcpServer = new VertxTcpServer();
         vertxTcpServer.doStart(rpcConfig.getServerPort());
+         */
     }
 }
