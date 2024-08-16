544e2421fe7c026fe98387c1a5f6e9d2433e0736
WolverineX23 <547036318@qq.com>
2024-08-16 20:25:13 +0800

    add：Rpc 框架启动和初始化 注解实现

---
+++ wx-rpc-spring-boot-starter/src/main/java/com/wx/wxrpc/springboot/starter/bootstrap/RpcInitBootstrap.java
@@ -0,0 +1,46 @@
+package com.wx.wxrpc.springboot.starter.bootstrap;
+
+import com.wx.rpc.RpcApplication;
+import com.wx.rpc.config.RpcConfig;
+import com.wx.rpc.server.tcp.VertxTcpServer;
+import com.wx.wxrpc.springboot.starter.annotation.EnableRpc;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.support.BeanDefinitionRegistry;
+import org.springframework.context.annotation.ImportBeanDefinitionRegistrar;
+import org.springframework.core.type.AnnotationMetadata;
+
+/**
+ * RPC 框架启动
+ *
+ * 从 Spring 元信息中获取到了 EnableRpc 注解的 needServer 属性，并通过它来判断是否需要启动服务器
+ */
+@Slf4j
+public class RpcInitBootstrap implements ImportBeanDefinitionRegistrar {
+
+    /**
+     * Spring 初始化时执行，初始化 RPC 框架
+     *
+     * @param importingClassMetadata
+     * @param registry
+     */
+    @Override
+    public void registerBeanDefinitions(AnnotationMetadata importingClassMetadata, BeanDefinitionRegistry registry) {
+        // 获取 EnableRpc 注解的属性值
+        boolean needServer = (boolean) importingClassMetadata.getAnnotationAttributes(EnableRpc.class.getName())
+                .get("needServer");
+
+        // RPC 框架初始化（加载 rpc 配置和注册中心初始化）
+        RpcApplication.init();
+
+        // 全局配置
+        final RpcConfig rpcConfig = RpcApplication.getRpcConfig();
+
+        // 启动服务器
+        if (needServer) {
+            VertxTcpServer vertxTcpServer = new VertxTcpServer();
+            vertxTcpServer.doStart(rpcConfig.getServerPort());
+        } else {
+            log.info("不启动 server");
+        }
+    }
+}
